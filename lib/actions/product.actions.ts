16bfd314d7e6f2a33bd436c642856b48d6a8c2de
bilgekaana <berberoglubilgekaan@gmail.com>
2025-02-03 17:16:38 +0300

    search filtering!

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -186,3 +186,24 @@ export async function updateProduct(
     };
   }
 }
+
+// Get all categories
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ["category"],
+    _count: true,
+  });
+
+  return data;
+}
+
+// Get featured products
+export async function getFeaturedProducts() {
+  const data = await prisma.product.findMany({
+    where: { isFeatured: true },
+    orderBy: { createdAt: "desc" },
+    take: 4,
+  });
+
+  return convertToPlainObject(data);
+}
